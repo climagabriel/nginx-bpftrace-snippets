4239bd6d4871c7ba970b6d5c501df85afff35dd3
Gabriel Clima <gabriel.clima@gcore.com>
2025-08-26 11:23:42 +0000

    save progress

--- event_loop_histogram_c/event_loop_histogram.bpf.c
+++ event_loop_histogram_c/event_loop_histogram.bpf.c
@@ -8,14 +8,21 @@ struct {
     __uint(max_entries, 10240);
     __type(key, __u32);
     __type(value, __u64);
-} counter_map SEC(".maps");
+} start_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, __u32);
+    __type(value, __u64);
+} duration_map SEC(".maps");
+
 
 SEC("tracepoint/syscalls/sys_exit_epoll_wait")
-int trace_epoll_wait(void *ctx)
+int exit_epoll_wait(void *ctx)
 {
     __u32 pid;
-    __u64 *value, newval;
-    __u64 initial_value = 1;
+    __u64 ts;
     char comm[6];
 
     bpf_get_current_comm(comm, sizeof(comm));
@@ -25,15 +32,31 @@ int trace_epoll_wait(void *ctx)
         return 0;
 
     pid = bpf_get_current_pid_tgid() >> 32;
+    ts = bpf_ktime_get_ns();
 
-    value = bpf_map_lookup_elem(&counter_map, &pid);
+    bpf_map_update_elem(&start_map, &pid, &ts, BPF_ANY);
+    return 0;
+}
 
-    if (value) {
-        newval = (*value) + 1;
-        bpf_map_update_elem(&counter_map, &pid, &newval, BPF_ANY);
-    } else {
-        bpf_map_update_elem(&counter_map, &pid, &initial_value, BPF_ANY);
-    }
+SEC("uretprobe")
+int BPF_UPROBE(ngx_ev_loop_end)
+{
+    __u32 pid;
+    __u64 start_ts, current_ts, duration_us;
+    void *start_ts_p;
+
+    pid = bpf_get_current_pid_tgid() >> 32;
+
+    start_ts_p = bpf_map_lookup_elem(&start_map, &pid);
+    if (!start_ts_p)
+        return 0;
+    start_ts = *(__u64 *)start_ts_p;
+    current_ts = bpf_ktime_get_ns();
+
+    duration_us = (current_ts - start_ts) / 1000;
+
+    bpf_map_update_elem(&duration_map, &pid, &duration_us, BPF_ANY);
+    bpf_map_delete_elem(&start_map, &pid);
 
     return 0;
 }
--- event_loop_histogram_c/event_loop_histogram_loader.c
+++ event_loop_histogram_c/event_loop_histogram_loader.c
@@ -2,33 +2,48 @@
 #include <bpf/bpf.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <libelf.h>
+
+#define RETPROBE 1
+#define ALL_PIDS -1
 
 int main() {
     struct bpf_object *obj;
-    struct bpf_program *prog;
-    struct bpf_map *map;
-    int map_fd;
+    struct bpf_program *exit_epoll_wait, *ngx_ev_loop_end;
+    struct bpf_map *start_map, *duration_map;
+    int start_map_fd, duration_map_fd;
+    const char *path = "/usr/sbin/nginx";
+    __u64 ngxoffset = 0x00000000002049e0;
+    struct bpf_link *link;
 
     obj = bpf_object__open_file("objs/event_loop_histogram.bpf.o", NULL);
     bpf_object__load(obj);
 
-    prog = bpf_object__find_program_by_name(obj, "trace_epoll_wait");
-    bpf_program__attach(prog);
+    exit_epoll_wait = bpf_object__find_program_by_name(obj, "exit_epoll_wait");
+    bpf_program__attach(exit_epoll_wait);
 
-    map = bpf_object__find_map_by_name(obj, "counter_map");
-    map_fd = bpf_map__fd(map);
+    start_map = bpf_object__find_map_by_name(obj, "start_map");
+    start_map_fd = bpf_map__fd(start_map);
+
+    ngx_ev_loop_end = bpf_object__find_program_by_name(obj, "ngx_ev_loop_end");
+    link = bpf_program__attach_uprobe(ngx_ev_loop_end, RETPROBE, ALL_PIDS, path,
+                                                                     ngxoffset);
+
+    duration_map = bpf_object__find_map_by_name(obj, "duration_map");
+    duration_map_fd = bpf_map__fd(duration_map);
+
+    //struct bpf_link * bpf_program__attach_uprobe(const struct bpf_program *prog, bool retprobe, pid_t pid, const char *binary_path, size_t func_offset);
 
     while (1) {
         __u32 key = 0, next_key;
         __u64 value;
 
-        while (bpf_map_get_next_key(map_fd, &key, &next_key) == 0) {
-            bpf_map_lookup_elem(map_fd, &next_key, &value);
-            printf("PID %u: %llu calls\n", next_key, value);
+        while (bpf_map_get_next_key(duration_map_fd, &key, &next_key) == 0) {
+            bpf_map_lookup_elem(duration_map_fd, &next_key, &value);
+            printf("PID %u: %llu usec\n", next_key, value);
             key = next_key;
         }
         sleep(1);
-            printf("\n");
     }
 
     return 0;
